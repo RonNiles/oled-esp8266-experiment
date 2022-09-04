1f4ad2d4add4bfb693f987425996d856f6a64224
RonNiles <ron.niles@memverge.com>
2022-09-04 10:29:46 -0700

    Woring on Interactive mode

--- oled_sketch_sep03a.ino
+++ oled_sketch_sep03a.ino
@@ -1,33 +1,77 @@
 #include <U8g2lib.h>
 
 U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the DispcdIcon  
+
 void setup() {
-  pinMode(D5, INPUT_PULLUP);
-
-  pinMode(D7, INPUT_PULLUP);
-
-  pinMode(D6, OUTPUT);
-  digitalWrite(D6, LOW);
+  pinMode(D8, INPUT_PULLUP);
   u8g2.begin();
+  u8g2.setPowerSave(1);  /* OLED power off */
 }
 
-void draw() {
+bool SelectOption(char *opt) {
+  for (int i = 0; i < 4; ++i) {
+    u8g2.firstPage();
+    do {
+      u8g2.drawStr(0, 0, opt);
+      u8g2.drawBox(i*32,10,32,6);
+    } while( u8g2.nextPage());
+    for (int j = 0; j < 5; ++j) {
+      delay(100);
+    if (digitalRead(D8) == HIGH)
+      return true;
+    }
+  }
+  return false;
+}
+
+void RunPump() {
+  u8g2.firstPage();
+  do {
+    u8g2.drawStr(0, 0, "Running Pump");
+  } while(u8g2.nextPage());
+  delay(1000);
+}
+
+void RunFeeder() {
+  u8g2.firstPage();
+  do {
+    u8g2.drawStr(0, 0, "Running Feeder");
+  } while( u8g2.nextPage());
+  delay(1000);
+}
+
+void ShowInformation() {
+  char buf[64];
+  sprintf(buf, "Millis: %lu\n", millis());
+  u8g2.firstPage();
+  do {
+    u8g2.drawStr(0, 0, buf);
+  } while( u8g2.nextPage());
+  delay(1000);
+}
+
+void Interactive() {
+  u8g2.setPowerSave(0);  /* OLED power on */
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.setFontRefHeightExtendedText();
   u8g2.setDrawColor(1);
-  if (digitalRead(D5) == LOW) {
-    u8g2.drawDisc(10,18,9);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+
+  if (SelectOption("Pump Manual Control")) {
+    RunPump();
+  } else if (SelectOption("Feeder Manual Control")) {
+    RunFeeder();
+  } else if (SelectOption("Show Information")) {
+    ShowInformation();
   }
-  if (digitalRead(D7) == LOW) {
-    u8g2.drawDisc(20,18,9);
-  }
+
+  u8g2.setPowerSave(1);  /* OLED power off */
 }
 
 void loop() {
-  // picture loop  
-  u8g2.firstPage();  
-  do {
-    draw();
-  } while( u8g2.nextPage() );
-  
-  // delay between each page
+  if (digitalRead(D8) == LOW)
+    return;
+  Interactive();
   delay(150);
 }
