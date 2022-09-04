171b9f5f5a76199ff975b9e87db14e9aae8c3fbe
RonNiles <ron.niles@memverge.com>
2022-09-04 01:05:19 -0700

    Inputs and OLED output

--- oled_sketch_sep03a.ino
+++ oled_sketch_sep03a.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <U8g2lib.h>
 
+U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the DispcdIcon  
+void setup() {
+  pinMode(D5, INPUT_PULLUP);
+
+  pinMode(D7, INPUT_PULLUP);
+
+  pinMode(D6, OUTPUT);
+  digitalWrite(D6, LOW);
+  u8g2.begin();
+}
+
+void draw() {
+  u8g2.setDrawColor(1);
+  if (digitalRead(D5) == LOW) {
+    u8g2.drawDisc(10,18,9);
+  }
+  if (digitalRead(D7) == LOW) {
+    u8g2.drawDisc(20,18,9);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // picture loop  
+  u8g2.firstPage();  
+  do {
+    draw();
+  } while( u8g2.nextPage() );
+  
+  // delay between each page
+  delay(150);
 }
